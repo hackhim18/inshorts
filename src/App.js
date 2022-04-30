d285d2e63488aa6f6d199c75ded4f73f0e5f26a3
hackhim18 <triphimanshu18@gmail.com>
2022-04-30 13:09:25 +0530

    c3-30/04

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import Header from './components/Header'
+import { Box, styled } from '@mui/material';
 
-function App()  {
+//components
+import Header from './components/Header';
+import InfoHeader from './components/InfoHeader';
+//import Articles from './components/Articles';
+
+
+const Container = styled(Box)(({ theme }) => ({
+  width: '59%',
+  margin: '110px auto 0 auto',
+  [theme.breakpoints.down('md')]: {
+    width: '75%'
+  },
+  [theme.breakpoints.down('sm')]: {
+    width: '85%'
+  }
+}));
+
+function App() {
+
   return (
-    <div>
+    <Box>
       <Header />
-    </div> 
-  )
-};
+      <Container>
+        <InfoHeader />
+        
+      </Container>
+    </Box>
+  );
+}
 
 export default App;
