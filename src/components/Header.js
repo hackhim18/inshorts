ad76b209ba4fb4fece52a5d0d6c1bc44e6b55c93
hackhim18 <triphimanshu18@gmail.com>
2022-04-30 12:52:21 +0530

    c2-30/04

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,33 @@
-import React from 'react'
-import { AppBar,Toolbar } from '@mui/material'
+import React from 'react';
+import  { AppBar, Toolbar, styled } from '@mui/material';
+import { Menu } from '@mui/icons-material';
 
- const Header = () => {
-  return (  
-      <AppBar>
-          <Toolbar>
+const StyledApp = styled(AppBar)`
+        background: #fff;
+        height: 70px;
+`;
+    
+const MenuIcon = styled(Menu)`
+        color: #000;
+`; 
+    
+const Image = styled('img')({
+        height: 55,
+        margin: 'auto',
+        paddingRight: 70
+})
 
-          </Toolbar>
-      </AppBar>
-  )
+const Header = () => {
+    const url = 'https://assets.inshorts.com/website_assets/images/logo_inshorts.png';
+
+    return (
+        <StyledApp>
+            <Toolbar>
+                <MenuIcon />
+                <Image src={url} alt="logo" />
+            </Toolbar>
+        </StyledApp>
+    )
 }
+
 export default Header;
