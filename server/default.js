423c0e56b770b5dff5820d09c61936ec37361958
Himanshu Tripathi <triphimanshu18@gmail.com>
2022-05-11 23:19:42 +0530

    Add files via upload

---
+++ server/default.js
@@ -0,0 +1,15 @@
+import News from './model/news-schema.js';
+import { data } from './constants/data.js';
+
+const DefaultData = async () => {
+    try {
+        await News.deleteMany({});
+        await News.insertMany(data);
+        console.log('Data imported Successfully');
+        
+    } catch (error) {
+        console.log('Error: ', error.message);
+    }
+}  
+
+export default DefaultData;  
